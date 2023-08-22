580b9df69edfc89fd0443bf6170760dbceea266d
Cendrine <cendrine.meystre@bedag.ch>
2023-08-22 16:20:17 +0200

    created spring calculator

---
+++ spring/calculator/src/test/java/la/ch/bedag/calculator/CalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package la.ch.bedag.calculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
